8f3c0e3cc1208cbf320af8cec095f06a1a48baea
Joe Rice <joerice123455t@gmail.com>
2026-02-03 23:58:33 +0000

    Fix Dlp Bugs

--- src/core/hle/service/nwm/nwm_uds.cpp
+++ src/core/hle/service/nwm/nwm_uds.cpp
@@ -614,101 +614,7 @@ void NWM_UDS::Shutdown(Kernel::HLERequestContext& ctx) {
     LOG_DEBUG(Service_NWM, "called");
 }
 
-void NWM_UDS::RecvBeaconBroadcastData(Kernel::HLERequestContext& ctx) {
-    IPC::RequestParser rp(ctx);
 
-    u32 out_buffer_size = rp.Pop<u32>();
-
-    // scan input struct
-    u32 unk1 = rp.Pop<u32>();
-    u32 unk2 = rp.Pop<u32>();
-
-    MacAddress mac_address;
-    rp.PopRaw(mac_address);
-
-    // uninitialized data in scan input struct
-    rp.Skip(9, false);
-
-    // end scan input struct
-
-
-    u32 wlan_comm_id = rp.Pop<u32>();
-    u32 id = rp.Pop<u32>();
-    // From 3dbrew:
-    // 'Official user processes create a new event handle which is then passed to this command.
-    // However, those user processes don't save that handle anywhere afterwards.'
-    // So we don't save/use that event too.
-    std::shared_ptr<Kernel::Event> input_event = rp.PopObject<Kernel::Event>();
-
-    Kernel::MappedBuffer out_buffer = rp.PopMappedBuffer();
-    ASSERT(out_buffer.GetSize() == out_buffer_size);
-
-    std::size_t cur_buffer_size = sizeof(BeaconDataReplyHeader);
-
-    // Retrieve all beacon frames that were received from the desired mac address.
-    auto beacons = GetReceivedBeacons(mac_address);
-
-    BeaconDataReplyHeader data_reply_header{};
-    data_reply_header.total_entries = static_cast<u32>(beacons.size());
-    data_reply_header.max_output_size = out_buffer_size;
-
-    // Write each of the received beacons into the buffer
-    for (const auto& beacon : beacons) {
-        BeaconEntryHeader entry{};
-        // TODO(Subv): Figure out what this size is used for.
-        entry.unk_size = static_cast<u32>(sizeof(BeaconEntryHeader) + beacon.data.size());
-        entry.total_size = static_cast<u32>(sizeof(BeaconEntryHeader) + beacon.data.size());
-        entry.wifi_channel = beacon.channel;
-        entry.header_size = sizeof(BeaconEntryHeader);
-        entry.mac_address = beacon.transmitter_address;
-
-        ASSERT(cur_buffer_size < out_buffer_size);
-
-        out_buffer.Write(&entry, cur_buffer_size, sizeof(BeaconEntryHeader));
-        cur_buffer_size += sizeof(BeaconEntryHeader);
-        const unsigned char* beacon_data = beacon.data.data();
-        out_buffer.Write(beacon_data, cur_buffer_size, beacon.data.size());
-        cur_buffer_size += beacon.data.size();
-    }
-
-    // Update the total size in the structure and write it to the buffer again.
-    data_reply_header.total_size = static_cast<u32>(cur_buffer_size);
-    out_buffer.Write(&data_reply_header, 0, sizeof(BeaconDataReplyHeader));
-
-    IPC::RequestBuilder rb = rp.MakeBuilder(1, 2);
-    rb.Push(ResultSuccess);
-    rb.PushMappedBuffer(out_buffer);
-
-    LOG_DEBUG(Service_NWM,
-              "called out_buffer_size=0x{:08X}, wlan_comm_id=0x{:08X}, id=0x{:08X},"
-              "unk1=0x{:08X}, unk2=0x{:08X}, offset={}",
-              out_buffer_size, wlan_comm_id, id, unk1, unk2, cur_buffer_size);
-}
-
-ResultVal<std::shared_ptr<Kernel::Event>> NWM_UDS::Initialize(
-    u32 sharedmem_size, const NodeInfo& node, u16 version,
-    std::shared_ptr<Kernel::SharedMemory> sharedmem) {
-
-    current_node = node;
-    initialized = true;
-
-    recv_buffer_memory = std::move(sharedmem);
-    ASSERT_MSG(recv_buffer_memory->GetSize() == sharedmem_size, "Invalid shared memory size.");
-
-    {
-        std::scoped_lock lock(connection_status_mutex);
-
-        // Reset the connection status, it contains all zeros after initialization,
-        // except for the actual status value.
-        connection_status = {};
-        connection_status.status = NetworkStatus::NotConnected;
-        node_info.clear();
-        node_info.push_back(current_node);
-        channel_data.clear();
-    }
-
-    return connection_status_event;
-}
 
 void NWM_UDS::InitializeWithVersion(Kernel::HLERequestContext& ctx) {
     IPC::RequestParser rp(ctx);
@@ -1326,6 +1232,83 @@ void NWM_UDS::ConnectToNetwork(Kernel::HLERequestContext& ctx, u16 command_id,
     connection_event = ctx.SleepClientThread("uds::ConnectToNetwork", UDSConnectionTimeout,
                                              std::make_shared<ThreadCallback>(command_id));
 }
+
+void NWM_UDS::RecvBeaconBroadcastData(Kernel::HLERequestContext& ctx) {
+    IPC::RequestParser rp(ctx);
+
+    u32 out_buffer_size = rp.Pop<u32>();
+
+    // scan input struct
+    u32 unk1 = rp.Pop<u32>();
+    u32 unk2 = rp.Pop<u32>();
+
+    MacAddress mac_address;
+    rp.PopRaw(mac_address);
+
+    // uninitialized data in scan input struct
+    rp.Skip(9, false);
+
+    // end scan input struct
+
+    u32 wlan_comm_id = rp.Pop<u32>();
+    u32 id = rp.Pop<u32>();
+    // From 3dbrew:
+    // 'Official user processes create a new event handle which is then passed to this command.
+    // However, those user processes don't save that handle anywhere afterwards.'
+    // So we don't save/use that event too.
+    std::shared_ptr<Kernel::Event> input_event = rp.PopObject<Kernel::Event>();
+
+    Kernel::MappedBuffer out_buffer = rp.PopMappedBuffer();
+    ASSERT(out_buffer.GetSize() == out_buffer_size);
+
+    std::size_t cur_buffer_size = sizeof(BeaconDataReplyHeader);
+
+    // on a real 3ds this is about 0.38 seconds
+    static constexpr std::chrono::nanoseconds UDSBeaconScanInterval{300000000};
+
+    input_event = ctx.SleepClientThread("uds::RecvBeaconBroadcastData", UDSBeaconScanInterval,
+                                             std::make_shared<ThreadCallback>(0xF));
+
+    // Retrieve all beacon frames that were received from the desired mac address.
+    auto beacons = GetReceivedBeacons(mac_address);
+
+    BeaconDataReplyHeader data_reply_header{};
+    data_reply_header.total_entries = static_cast<u32>(beacons.size());
+    data_reply_header.max_output_size = out_buffer_size;
+
+    // Write each of the received beacons into the buffer
+    for (const auto& beacon : beacons) {
+        BeaconEntryHeader entry{};
+        // TODO(Subv): Figure out what this size is used for.
+        entry.unk_size = static_cast<u32>(sizeof(BeaconEntryHeader) + beacon.data.size());
+        entry.total_size = static_cast<u32>(sizeof(BeaconEntryHeader) + beacon.data.size());
+        entry.wifi_channel = beacon.channel;
+        entry.header_size = sizeof(BeaconEntryHeader);
+        entry.mac_address = beacon.transmitter_address;
+
+        ASSERT(cur_buffer_size < out_buffer_size);
+
+        out_buffer.Write(&entry, cur_buffer_size, sizeof(BeaconEntryHeader));
+        cur_buffer_size += sizeof(BeaconEntryHeader);
+        const unsigned char* beacon_data = beacon.data.data();
+        out_buffer.Write(beacon_data, cur_buffer_size, beacon.data.size());
+        cur_buffer_size += beacon.data.size();
+    }
+
+    // Update the total size in the structure and write it to the buffer again.
+    data_reply_header.total_size = static_cast<u32>(cur_buffer_size);
+    out_buffer.Write(&data_reply_header, 0, sizeof(BeaconDataReplyHeader));
+
+    IPC::RequestBuilder rb = rp.MakeBuilder(1, 2);
+    rb.Push(ResultSuccess);
+    rb.PushMappedBuffer(out_buffer);
+
+    LOG_DEBUG(Service_NWM,
+              "called out_buffer_size=0x{:08X}, wlan_comm_id=0x{:08X}, id=0x{:08X},"
+              "unk1=0x{:08X}, unk2=0x{:08X}, offset={}",
+              out_buffer_size, wlan_comm_id, id, unk1, unk2, cur_buffer_size);
+}
+
 
 void NWM_UDS::ConnectToNetwork(Kernel::HLERequestContext& ctx) {
     IPC::RequestParser rp(ctx);
